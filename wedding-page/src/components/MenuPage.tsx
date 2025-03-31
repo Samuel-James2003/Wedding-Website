7c24d72cd00b4e7e0379924240f47a3a707f50c9
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2025-03-31 08:15:19 -0400

    refactor: improve formatting and structure of MenuPage and SeatingChart components

--- wedding-page/src/components/MenuPage.tsx
+++ wedding-page/src/components/MenuPage.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Container, Typography, List, ListItem, ListItemText, Divider, Paper, Box } from '@mui/material';
+import React from "react";
+import {
+  Container,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Paper,
+  Box,
+} from "@mui/material";
 
 const MenuPage: React.FC = () => {
   return (
@@ -12,11 +21,21 @@ const MenuPage: React.FC = () => {
         <Box mb={3}>
           <Typography variant="h5">Apps</Typography>
           <List>
-            <ListItem><ListItemText primary="Mini grilled cheese with duck" /></ListItem>
-            <ListItem><ListItemText primary="Mini quiche" /></ListItem>
-            <ListItem><ListItemText primary="Salmon canape" /></ListItem>
-            <ListItem><ListItemText primary="Spanakopita" /></ListItem>
-            <ListItem><ListItemText primary="Charcuterie & Crudité board" /></ListItem>
+            <ListItem>
+              <ListItemText primary="Mini grilled cheese with duck" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Mini quiche" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Salmon canape" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Spanakopita" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Charcuterie & Crudité board" />
+            </ListItem>
           </List>
         </Box>
 
@@ -25,7 +44,9 @@ const MenuPage: React.FC = () => {
         <Box my={3}>
           <Typography variant="h5">Salad</Typography>
           <List>
-            <ListItem><ListItemText primary="House Salad" /></ListItem>
+            <ListItem>
+              <ListItemText primary="House Salad" />
+            </ListItem>
           </List>
         </Box>
 
@@ -34,7 +55,9 @@ const MenuPage: React.FC = () => {
         <Box my={3}>
           <Typography variant="h5">Pasta Course</Typography>
           <List>
-            <ListItem><ListItemText primary="Penne alla vodka" /></ListItem>
+            <ListItem>
+              <ListItemText primary="Penne alla vodka" />
+            </ListItem>
           </List>
         </Box>
 
@@ -43,8 +66,12 @@ const MenuPage: React.FC = () => {
         <Box my={3}>
           <Typography variant="h5">Entrees</Typography>
           <List>
-            <ListItem><ListItemText primary="Grilled Salmon" /></ListItem>
-            <ListItem><ListItemText primary="Chicken marsala" /></ListItem>
+            <ListItem>
+              <ListItemText primary="Grilled Salmon" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Chicken marsala" />
+            </ListItem>
           </List>
         </Box>
 
@@ -53,8 +80,12 @@ const MenuPage: React.FC = () => {
         <Box my={3}>
           <Typography variant="h5">Sides</Typography>
           <List>
-            <ListItem><ListItemText primary="Roasted Rosemary potatoes" /></ListItem>
-            <ListItem><ListItemText primary="Asparagus" /></ListItem>
+            <ListItem>
+              <ListItemText primary="Roasted Rosemary potatoes" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Asparagus" />
+            </ListItem>
           </List>
         </Box>
 
@@ -63,10 +94,18 @@ const MenuPage: React.FC = () => {
         <Box mt={3}>
           <Typography variant="h5">Dessert</Typography>
           <List>
-            <ListItem><ListItemText primary="Cake (our own)" /></ListItem>
-            <ListItem><ListItemText primary="Cookies (our own)" /></ListItem>
-            <ListItem><ListItemText primary="Espresso" /></ListItem>
-            <ListItem><ListItemText primary="Coffee decaf" /></ListItem>
+            <ListItem>
+              <ListItemText primary="Cake (our own)" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Cookies (our own)" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Espresso" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Coffee decaf" />
+            </ListItem>
           </List>
         </Box>
       </Paper>
