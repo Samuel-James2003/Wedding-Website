f61dcd7581be91c8d0d3c1482ea0a89527976152
Samuel James <64286580+Samuel-James2003@users.noreply.github.com>
2025-05-10 06:30:37 -0400

    feat: add SVG flower graphic and welcome page frame image; update index.html and index.tsx for typography theme

--- wedding-page/src/index.tsx
+++ wedding-page/src/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontFamily: "'Great Vibes', cursive",
+    },
+    h2: {
+      fontFamily: "'Great Vibes', cursive",
+    },
+    h3: {
+      fontFamily: "'Great Vibes', cursive",
+    },
+    h4: {
+      fontFamily: "'Great Vibes', cursive",
+    },
+  },
+});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
